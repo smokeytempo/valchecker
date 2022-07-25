ad63af8226863f3a9fbb8f4079f1659bfe0ca7b5
LIL-JABA <82034934+LIL-JABA@users.noreply.github.com>
2022-07-25 10:36:23 +0300

    v 2.1.0 (check description)
    you can now choose the default file. you can also choose will checker automatically save output to out.txt or no. if you found bugs please open an issue

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+
 import requests
 from modules import systems
 from modules import checkers
 import ctypes
 import os
+from tkinter import filedialog
+import tkinter
+import json
 
 check=checkers.checkers()
 sys=systems.system()
@@ -11,7 +15,7 @@ class program():
     def __init__(self) -> None:
         self.count=0
         self.checked=0
-        self.version='2.0.2'
+        self.version='2.1.0'
         try:
             self.lastver=requests.get('https://lil-jaba.github.io/valchecker/system/lastver.html').text.replace(' ','').replace('\n','')
             if 'a' in self.lastver:
@@ -21,6 +25,7 @@ class program():
 
     def start(self):
         while True:
+            ctypes.windll.kernel32.SetConsoleTitleW(f'ValChecker {self.version} by liljaba1337')
             os.system('cls')
             print(sys.center(f'''
 
@@ -34,7 +39,7 @@ class program():
             print(sys.center(f'v{self.version}'))
             if self.lastver!=self.version:
                 print(sys.center(f'update to the last version ({self.lastver}) on my GitHub'))
-            print(sys.center('\nhttps://github.com/LIL-JABA\n'))
+            print(sys.center('\nhttps://github.com/LIL-JABA/valchecker\n'))
             print('  [1] - START CHECKER')
             print('  [2] - EDIT SETTINGS')
             res=str(input('\n>>>'))
@@ -45,28 +50,47 @@ class program():
             elif res=='2':
                 sys.edit_settings()
             else:
-                pr.main()
-                break
+                continue
 
 
     def get_accounts(self,filename):
-        try:
-            with open (str(filename)+'.txt', 'r', encoding='UTF-8') as file:
-                lines = file.readlines()
-                ret=[]
-                for logpass in lines:
-                    self.count+=1
-                    logpass=logpass.split(' - ')[0].replace('\n','').replace(' ','')
-                    ret.append(logpass)
-                print(f'detected {self.count} accounts\n')
-                return ret
-        except:
-            return 0
+        while True:
+            os.system('cls')
+            try:
+                with open (str(filename), 'r', encoding='UTF-8') as file:
+                    lines = file.readlines()
+                    ret=[]
+                    for logpass in lines:
+                        self.count+=1
+                        logpass=logpass.split(' - ')[0].replace('\n','').replace(' ','')
+                        ret.append(logpass)
+                    print(f'detected {self.count} accounts\n')
+                    return ret
+            except:
+                print(f"can't find the default file ({filename})\nplease select a new one")
+                root = tkinter.Tk()
+                file = filedialog.askopenfile(parent=root, mode='rb', title='select file with accounts (login:password)',
+                    filetype=(("txt", "*.txt"), ("All files", "*.txt")))
+                root.destroy()
+                if file==None:
+                    print('u chose nothing')
+                    input('press ENTER to choose again')
+                    continue
+                filename=str(file).split("name='")[1].split("'>")[0]
+                with open('system\\settings.json','r+') as f:
+                    data = json.load(f)
+                    data['default_file']=filename
+                    f.seek(0)
+                    json.dump(data, f, indent=4)
+                    f.truncate()
+                continue
+
 
     def main(self):
-        checkru=sys.load_settings()
+        settings=sys.load_settings()
+        checkru=settings['checkru']
+        fn=settings['default_file']
         tofile=''
-        fn='accounts'
         accounts=self.get_accounts(fn)
         if accounts==0:
             print('file "accounts.txt" was not found')
@@ -109,10 +133,14 @@ class program():
             tofile+=f'last game was on {lp}\n'
             self.checked+=1
             ctypes.windll.kernel32.SetConsoleTitleW(f'ValChecker {self.version} by liljaba1337 | CHECKED {self.checked}/{self.count}')
-        saveno=str(input('save output to "out.txt"? (y/n) >>>'))
-        if saveno=='y':
+        if settings['saveout']=='False':
+            saveno=str(input('save output to "out.txt"? (y/n) >>>'))
+        else:
+            saveno='y'
+        if saveno=='y' or settings['saveout']=='True':
             with open ('out.txt', 'w', encoding='UTF-8') as file:
                 file.write(tofile)
+            print('saved output to out.txt')
         else:
             pass
     
--- modules/systems.py
+++ modules/systems.py
@@ -4,6 +4,8 @@ import asyncio
 import sys
 import json
 import os
+from tkinter import filedialog
+import tkinter
 
 class system():
     def auth(self,login=None,password=None,logpass=None,response=4):
@@ -62,12 +64,12 @@ class system():
         try:
             f = open('system\\settings.json')
             data = json.load(f)
-            checkru=data['ru_check']
+            out={'checkru':data['ru_check'],'default_file':data['default_file'],'saveout':data['saveout']}
             f.close()
+            return out
         except:
-            print("can't find settings.json\nusung default settings\n")
-            checkru='True'
-        return checkru
+            print("can't find settings.json\nplease download it from my github\n")
+            return False
 
     def edit_settings(self):
         while True:
@@ -75,14 +77,33 @@ class system():
             f = open('system\\settings.json','r+')
             data = json.load(f)
             checkru=data['ru_check']
+            deffile=data['default_file']
+            saveout=data['saveout']
             print(f'  [1] check skins in russian: {checkru}')
-            print(f'  [~] enter any other number to exit')
+            print(f'  [2] default file: {deffile}')
+            print(f'  [3] always save output to out.txt: {saveout}')
+            print(f'\n  [~] enter any other number to exit')
             edit=str(input('\nenter the number u want to turn on/off >>>')).replace(' ','')
             if edit=='1':
                 if checkru=='True':
                     data['ru_check']='False'
                 else:
                     data['ru_check']='True'
+            elif edit=='2':
+                root = tkinter.Tk()
+                file = filedialog.askopenfile(parent=root, mode='rb', title='select file with accounts (login:password)',
+                    filetype=(("txt", "*.txt"), ("All files", "*.txt")))
+                root.destroy()
+                if file==None:
+                    filename='None'
+                else:
+                    filename=str(file).split("name='")[1].split("'>")[0]
+                data['default_file']=filename
+            elif edit=='3':
+                if saveout=='True':
+                    data['saveout']='False'
+                else:
+                    data['saveout']='True'
             else:
                 return
             f.seek(0)
