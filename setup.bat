cca65286fa813a7612cff49e59341261c746d211
balls <gafarovalexander@gmail.com>
2024-04-17 23:58:25 +0300

    requirements uwu

--- setup.bat
+++ setup.bat
@@ -1,11 +1,5 @@
 @echo off
 
-pip install urllib3
-pip install requests
-pip install colorama
-pip install keyboard
-pip install inquirerpy
-pip install aiohttp
-pip install pywin32
+pip install -r requirements.txt
 
 pause
